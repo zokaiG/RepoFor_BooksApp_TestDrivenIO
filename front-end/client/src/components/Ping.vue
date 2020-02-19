3ab19f6b660943474b9f6c2d0a407944afc8327f
zokaiG <futlouco7@gmail.com>
2020-02-19 15:50:42 -0500

    Modified app to display Ping.vue

---
+++ front-end/client/src/components/Ping.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p> {{message}} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Ping',
+  data() {
+    return {
+      message: 'Hello!',
+    };
+  },
+
+};
+</script>
