1ad0a93351d07ee49ecaaf6fe3ec8445bc4aef9f
zokaiG <futlouco7@gmail.com>
2020-02-19 16:07:32 -0500

    Connected the FrontEnd with the BackEnd

--- front-end/client/src/components/Ping.vue
+++ front-end/client/src/components/Ping.vue
@@ -5,13 +5,30 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Ping',
   data() {
     return {
-      message: 'Hello!',
+      message: '',
     };
   },
-
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5000/ping';
+      axios.get(path)
+        .then((res) => {
+          this.message = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
 };
 </script>
