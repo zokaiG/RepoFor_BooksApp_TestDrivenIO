a18dfc1f80aa54089099bb5f8e4d8e5b7c3e641d
zokaiG <futlouco7@gmail.com>
2020-02-20 12:52:42 -0500

    Added Books list in the backend and started serving it in json format via all_books() GET Request

--- back-end/app.py
+++ back-end/app.py
@@ -11,11 +11,37 @@ app.config.from_object(__name__)
 # Enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+Books = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter and the Philosopher\'s Stone',
+        'author': 'J. K. Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
 
 # Sanity Check Route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+
+@app.route('/books', methods=['GET'])
+def all_books():
+    return jsonify({
+        'status': 'success',
+        'books': Books
+    })
 
 
 if __name__ == '__main__':
